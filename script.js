848f48f6bc485e268c2306d2532f230959ad2c4a
estudianteUC <estudianteUC@UCUXB100103.continental.edu.pe>
2026-05-29 17:01:08 -0500

    java script agregado

---
+++ script.js
@@ -0,0 +1,8 @@
+const num1=document.getElementById('num1');
+const num2=document.getElementById('num2');
+const sumBtn=document.getElementById('sumBtn');
+const result=document.getElementById('result');
+sumBtn.addEventListener('click',()=>{
+    const total=Number(num1.value)+Number(num2.value);
+    result.textContent=`Resultado: ${total}`;
+});

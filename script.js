498183db2e9cf11a851c9113b34473a4577b6487
estudianteUC <estudianteUC@UCUXB100103.continental.edu.pe>
2026-05-29 17:06:59 -0500

    error de script corregido

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const num2=document.getElementById('num2');
 const sumBtn=document.getElementById('sumBtn');
 const result=document.getElementById('result');
 sumBtn.addEventListener('click',()=>{
-    const total=Number(num1.value)+Numbernum2.value;
+    const total=Number(num1.value)+Number(num2.value);
     result.textContent=`Resultado: ${total}`;
 });
